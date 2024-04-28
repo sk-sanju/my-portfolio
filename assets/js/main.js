f2050e9c1727ee2b4668fc836709fc657e0fc5c9
sk-sanju <sanjayskpy1@gmail.com>
2024-04-29 00:16:52 +0530

    scroll element reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,3 +225,23 @@
   new PureCounter();
 
 })()
+
+    document.addEventListener('DOMContentLoaded', function() {
+      const sections = document.querySelectorAll('.reveal');
+      const options = {
+        threshold: 0.2
+      };
+
+      const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('reveal-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }, options);
+
+      sections.forEach(section => {
+        observer.observe(section);
+      });
+    });
