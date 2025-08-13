8d57ef338db57a492fdc78a1753a46e5f4ce1255
Sanjay S <133774180+sk-sanju@users.noreply.github.com>
2025-08-14 00:23:25 +0530

    Delete OneDrive/Documents/GitHub/xenotuneApp directory

--- OneDrive/Documents/GitHub/xenotuneApp/xeno_backend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HealthCheckView, APIRootView, AboutView
-
-urlpatterns = [
-    path('', APIRootView.as_view()),
-    path('health/', HealthCheckView.as_view()),
-    path('about/', AboutView.as_view()),
-]
--- OneDrive/Documents/GitHub/xenotuneApp/xeno_backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView, CustomLoginView, LogoutView, ProfileView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', CustomLoginView.as_view()),
-    path('logout/', LogoutView.as_view()),
-    path('profile/', ProfileView.as_view()),
-]
